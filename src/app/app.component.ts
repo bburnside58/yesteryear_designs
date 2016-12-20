0cb72fd524285fa4a458f6d7b9d3ba265461a4df
Brian Burnside <bcburnside58@gmail.com>
2016-12-20 14:58:41 -0500

    forms_start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import {
+	FormBuilder,
+	FormGroup,
+	Validators,
+	AbstractControl
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+	myForm: FormGroup;
+	sku: any;
+	
+	constructor(fb: FormBuilder) { 
+		this.myForm = fb.group({
+      	'sku':  ['sku', Validators.required]
+    	});
+	
+		this.sku = this.myForm.controls['sku']; 
+
+	}
+	
+	onSubmit(value: string): void { 
+		console.log('you submitted value: ', value);
+	} 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { 
+    FormsModule,
+    ReactiveFormsModule
+ } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+
+
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { AttentionComponent } from './attention/attention.component';
+import { ContentComponent } from './content/content.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainComponent,
+    AttentionComponent,
+    ContentComponent,
+    AboutComponent,
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule
   ],
   providers: [],
   bootstrap: [AppComponent]
